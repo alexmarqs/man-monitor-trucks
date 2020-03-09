8586554d63a2142baad83e955eae0a8bc5511c88
Alexandre Marques <pt.alexandremarques@gmail.com>
2020-03-09 00:37:21 +0000

    Fix bug with view all poi type option

--- monitor-truck-client/src/services/googlePlacesApi.js
+++ monitor-truck-client/src/services/googlePlacesApi.js
@@ -2,7 +2,7 @@ export const searchNearby = (google, map, request) => {
   return new Promise((resolve, reject) => {
     const service = new google.maps.places.PlacesService(map);
     service.nearbySearch(request, (results, status) => {
-      if (status === google.maps.places.PlacesServiceStatus.OK) {
+      if (status === google.maps.places.PlacesServiceStatus.OK || status === google.maps.places.PlacesServiceStatus.ZERO_RESULTS) {
         resolve(results);
       } else {
         reject(status);
